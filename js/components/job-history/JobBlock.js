c1d28535927cfaff0e4419a2f2f8de1b268c1d88
Rimantas <belauzas@yahoo.com>
2020-05-26 15:18:47 +0300

    Job history section

--- js/components/job-history/JobBlock.js
+++ js/components/job-history/JobBlock.js
@@ -32,12 +32,46 @@ class JobBlock {
                         <h3>${this.data.jobTitle}</h3>
                         <p>${location}</p>
                     </div>
-                    <div class="btn">Jul'15</div>
+                    <div class="btn">${this.formatDate(this.data.duration)}</div>
                 </div>
                 <p>${this.data.description}</p>
             </div>`;
 
         this.parentDOM.insertAdjacentHTML('beforeend', HTML);
+    }
+
+    formatDate( date ) {
+        // Jul'15 to Dec'25
+        // Jul'15 to Present
+        // Jul'15 to Aug'18
+
+        const {from, untill} = date;
+
+        let time = '';
+        if ( from && typeof(from) === 'object' ) {
+            time += this.dateObjectToMonthYear(from);
+        } else {
+            time += 'Birth';
+        }
+
+        time += ' to ';
+
+        if ( untill && typeof(untill) === 'object' ) {
+            time += this.dateObjectToMonthYear(untill);
+        } else {
+            time += 'Present';
+        }
+
+        return time;
+    }
+
+    dateObjectToMonthYear( obj ) {
+        const {month, year} = obj;
+        const shortYear = year % 100;
+        const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        
+        return monthNames[month] + "'" + (shortYear > 9 ? shortYear : '0'+shortYear);
     }
 }
 
